6cbf9c009921a444a5f4edbc3742891c8be0dee4
Alfonso Robles <fons3517@gmail.com>
2021-10-14 16:20:56 -0500

    added code for 'seeds.sql'. Entered department name into its table; title, salary into its table.

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE emp_role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
+  salary DECIMAL (6, 2),
   department_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id)
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES  (Sales),
+        (Engineering),
+        (Finance),
+        (Legal);
+
+
+INSERT INTO emp_role (title, salary, department_id)
+VALUES  (Salesperson, 80000),
+        (Lead Engineer, 15000),
+        (Software Engineer, 120000),
+        (Account Manager, 160000),
+        (Accountant, 125000),
+        (Legal Team Lead, 250000),
+        (Lawyer, 190000);
