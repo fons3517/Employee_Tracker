d4f33f86de136cbfb4a805fc0048d4028c1b8d41
Alfonso Robles <fons3517@gmail.com>
2021-10-20 23:50:18 -0500

    added query.sql data

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,15 @@
+USE globadine_db;
+SELECT * FROM department;
+SELECT * FROM emp_role;
+SELECT * FROM employee;
+
+SELECT * 
+FROM emp_role
+INNER JOIN employee
+ON 
+emp_role.id = employee.id;
+
+UPDATE employee.role_id
+SET employee.role_id=${employee.role_id}
+WHERE role_id=${employee.role_id};
 
